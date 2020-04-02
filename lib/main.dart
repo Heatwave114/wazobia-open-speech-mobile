8becbcc1dd9acbcb0895428672ae26f3ebb31cca
Heatwave114 <heatwavemachine@gmail.com>
2020-04-02 19:46:32 +0100

    merging with remote orgin

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+// External
+import 'package:flutter/material.dart';
+
+// Internal
+import './screens/landing_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: Scaffold(
+        body: LandingScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/landing_screen.dart
@@ -0,0 +1,13 @@
+// External
+import 'package:flutter/material.dart';
+
+
+
+class LandingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+    );
+  }
+}
